98fca93d78eb90a4f656f2ffacee18fd5abdd28e
Ubuntu <ubuntu@ip-172-31-89-97.ec2.internal>
2019-06-04 15:07:06 +0000

    done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,34 @@
 // your Bomb code here!
+import React, {Component} from "react";
+
+class Bomb extends Component{
+    
+    constructor(props){
+        super(props)
+        this.state={
+            secondsLeft:props.initialCount
+        }
+    }
+    
+    
+    render(){
+        
+        let message
+        
+        if (this.state.secondsLeft===0){
+            message = 'Boom!'
+        } else{
+            message = `${this.state.secondsLeft} seconds left before I go boom!`
+        }
+        
+        return(
+            <div>
+                {message}
+            </div>
+            )
+    }
+    
+}
+
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,28 @@
 // your ImageSlider code here!
+import React, {Component} from "react";
+
+class ImageSlider extends Component{
+    
+    constructor(props){
+        super(props)
+        this.state={
+            currentSlideIndex: 0
+        }
+        
+    }
+    
+    
+    render(){
+        return(
+            <div
+                className="ImageSlider" 
+            >
+                <p>{`I am on slide ${this.state.currentSlideIndex}`}</p>
+            </div>
+            )
+    }
+    
+    
+}
+
+export default ImageSlider
